5ec2e7e6e09509579d713e2824144764ef4ef305
Harsh <80537315+harsh0757@users.noreply.github.com>
2023-07-24 18:24:28 +0000

    Linked List

--- LinkedList/doublyLL.cpp
+++ LinkedList/doublyLL.cpp
@@ -37,8 +37,55 @@ void InsertAtTail(DNode* &tail, int data){
     tail = temp;
 }
 
-void insertAtPosition(){
+void insertAtPosition(DNode* &head, DNode* &tail, int position, int data){
+    if(position == 1){
+        insertAtHead(head,data);
+        return;
+    }
+    DNode* temp = head;
+    int count = 1;
 
+    while(count < position-1){
+        temp = temp->next;
+        count++;
+    }
+
+    if(temp->next == NULL){
+        InsertAtTail(tail,data);
+        return;
+    }
+
+    DNode* NodeToInsert = new DNode(data);
+    NodeToInsert->next = temp->next;
+    temp->next->prev = NodeToInsert;
+    temp->next = NodeToInsert;
+    NodeToInsert->prev = temp;
+}
+
+void Delete(int position, DNode* &head){
+    if(position == 1){
+        DNode* temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    DNode* curr = head;
+    DNode* prev = NULL;
+
+    int count = 1;
+    if(count < position){
+        prev = curr;
+        curr = curr->next;
+        count++;
+    }
+    curr->prev = NULL;
+    prev->next = curr->next;
+    curr->next->prev = prev;
+    curr->next = NULL;
+
+    delete curr;
 }
 
 void print(DNode* head){
@@ -49,9 +96,21 @@ void print(DNode* head){
     DNode* temp = head;
     while(temp!=NULL){
         cout<<temp->data<<" ";
+        temp = temp->next;
     }
+    cout<<endl;
 }
 
 int main(){
-    
+    DNode* node1 = new DNode(10);
+    DNode* head = node1;
+    DNode* tail = node1;
+
+    insertAtHead(head,20);
+    print(head);
+    InsertAtTail(tail,30);
+    print(head);
+    insertAtPosition(head,tail,4,40);
+    print(head);
+
 }
