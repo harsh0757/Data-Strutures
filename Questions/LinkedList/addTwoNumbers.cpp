9f28384eabddf050befbc5b5f0f0b12efef7d14f
Harsh <80537315+harsh0757@users.noreply.github.com>
2024-02-02 12:31:30 +0000

    Linked List

---
+++ Questions/LinkedList/addTwoNumbers.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+// link : https://leetcode.com/problems/add-two-numbers/submissions/1163276762/
+int main() {
+    
+}
---
+++ Questions/LinkedList/oddEvenLinkedList.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //https://leetcode.com/problems/odd-even-linked-list/description/?source=submission-ac
+}
