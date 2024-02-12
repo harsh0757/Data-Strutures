2686b39088b947bc78699c85fd3868498e0448ee
Harsh <80537315+harsh0757@users.noreply.github.com>
2024-02-12 12:24:26 +0000

    Stack

---
+++ Questions/Stack/ImplementStackUsingQueue.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //https://leetcode.com/problems/implement-stack-using-queues/description/
+}
---
+++ Questions/Stack/twoStacks.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // https://www.codingninjas.com/studio/problems/two-stacks_983634?leftPanelTabValue=PROBLEM
+}
