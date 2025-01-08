40cd8c96cfaaebe3d09b9d72e7a9ddafbcfe1354
Harsh <80537315+harsh0757@users.noreply.github.com>
2025-01-08 12:20:17 +0000

    STL

---
+++ STL/Iterators/auto_Range_basedLoops.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v = {2,3,5,6,7};
+    vector<pair<int, int>> v_p = {{1,2}, {2,4}};
+//range based loops
+//we can take reference values also
+    for(int it : v){ //it will get the copy of the values
+        cout<<it<<" ";
+    }
+    cout<<endl;
+//on vector pairs
+    // for(pair<int,int> it : v_p){
+    for(auto it : v_p){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+    cout<<endl;
+//auto keyword
+    auto a = 1;
+    cout<<a<<endl;
+
+    for(auto it : v){  // we can use like this also
+        cout<<it<<" ";
+    }
+}
