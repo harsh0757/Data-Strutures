1b6897e81602be3c4d45893a52bf52f98e77ebf8
harshchanana <harshchanana38@gmail.com>
2025-08-15 01:15:54 +0530

    Questions

--- Stack/L5_Next_greater_element.cpp
+++ Stack/L5_Next_greater_element.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+vector<int> optimal(vector<int>& arr){
+    stack<int> st;
+    vector<int> ans(arr.size(), -1);
+
+    for(int i=arr.size();i>=0;i--){
+        while(!st.empty() && st.top() <= arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            ans[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+    return ans;
+}
+
 vector<int> brute_force(vector<int> arr){
     vector<int> ans(arr.size(), -1);
     for(int i=0;i<arr.size();i++){
@@ -17,7 +33,7 @@ vector<int> brute_force(vector<int> arr){
 int main(){
     vector<int> arr = {6,0,8,1,3};
     // return {8,8,-1,3,-1};
-    vector<int> ans = brute_force(arr);
+    vector<int> ans = optimal(arr);
     for(auto it : ans){
         cout<<it<<" ";
     } cout<<endl;
